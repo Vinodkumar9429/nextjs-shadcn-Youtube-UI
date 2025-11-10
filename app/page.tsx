31cbf00933a4b3a88201c0693cd1a37b7bd0604c
Vinodkumar9429 <umr09347@gmail.com>
2025-11-10 22:37:29 +0530

    Map video cards on to the UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,104 @@
 import Header from "../components/Header"
 import VideoCard from "../components/VideoCard"
 
+
+
+export interface videoType {
+    id:number,
+    imgSrc : string,
+    title:string,
+    channel : string,
+    views : string
+}
+
+const arrayRes:videoType[] = [
+    {   id:1,
+        imgSrc:"/p1.jpeg",
+        title:"Deadpool, but he gets increasingly comedic",
+        channel:"Marvel Nig",
+        views:"2.3M views. 1 year ago"
+    },
+    {   id:2,
+        imgSrc:"/p2.jpeg",
+        title:"Javascript survival guide, 101",
+        channel:"Programmer bro",
+        views:"50K views. 1 month ago"
+    },
+    {   id:3,
+        imgSrc:"/p3.jpg",
+        title:"Here is why reasons why BMW doesn't need a supercar",
+        channel:"Formula 2",
+        views:"431k views. 3 years ago"
+    },
+    {   id:4,
+        imgSrc:"/p4.jpg",
+        title:"How to buy a maybach in your twenties",
+        channel:"79 with sharan",
+        views:"4M views. 9 months ago"
+    },
+    {   id:5,
+        imgSrc:"/p5.jpg",
+        title:"Here are 10 times when anderson silva opponents claimed their insurance.",
+        channel:"UFC",
+        views:"23M views. 6 years ago"
+    },
+    {   id:6,
+        imgSrc:"/p6.webp",
+        title:"It is just an Anime...",
+        channel:"AyanoModi",
+        views:"2M views. 1 year ago"
+    },
+    {   id:7,
+        imgSrc:"/p7.webp",
+        title:"patience kaise gatayein.",
+        channel:"Sandeep Maheshwari",
+        views:"204K views. 1 year ago"
+    },
+    {   id:8,
+        imgSrc:"/p8.jpg",
+        title:"what happened to may after shadow fight 2 ?",
+        channel:"OTC",
+        views:"20M views. 5 years ago"
+    },
+    {   id:9,
+        imgSrc:"/p9.jpg",
+        title:"Mr beast bought Ishowspeed for 3$",
+        channel:"The Seasons",
+        views:"998K views. 5 days ago"
+    },
+    {   id:10,
+        imgSrc:"/p10.png",
+        title:"How to reverse a car, Leetcode 3209",
+        channel:"Algo Reps",
+        views:"198K views. 1 month ago"
+    },
+
+]
+
+
+
+
+
+
+
+
 const Page = () => {
   return (
     <div className="font-general-sans flex flex-col">
       <Header />
 
-      <div className="w-screen h-screen py-5 px-5 grid grid-cols-3">
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
+      <div className="w-screen h-screen py-5 px-5 grid grid-cols-3 gap-y-5">
+        {
+          arrayRes.map(obj=><VideoCard
+            id={obj.id}
+            key={obj.id}
+            imgSrc={obj.imgSrc}
+            title={obj.title}
+            channel={obj.channel}
+            views={obj.views}
+             />)
+        }
+
       </div>
     </div>
   )
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image"
+import { videoType } from "../app/page"
 
-const VideoCard = () => {
+
+
+const VideoCard = ({imgSrc, title, channel, views}:videoType) => {
   return (
-    <div className="w-[470px] h-72 border">
-        
+    <div className="w-[470px] h-[300px] border rounded-3xl overflow-hidden hover:bg-accent/70">
+        <Image
+        className="w-full h-[70%] bg-cover bg-center"
+         src={imgSrc} alt=""
+         height={400} width={500}
+         />
+         <div className="flex items-center px-2 py-2 gap-x-3">
+            <Image src={"/1.jpg"} alt=""
+            className="rounded-full w-14 h-14"
+            height={70} width={70}
+             />
+            <div>
+                <h1 className="font-bold text-sm">{title}</h1>
+                <p className="text-muted-foreground text-sm">{channel}</p>
+                <p className="text-muted-foreground text-sm">{views}</p>
+            </div>
+         </div>
     </div>
   )
 }
